ff52001668e2e3c3e618b38c6d143ae578400f0a
SebastianE <sebastian.ehmes@gmail.com>
2022-02-07 16:16:25 +0100

    + added function to add and remove variables by adding and removing corresponding matches

--- org.emoflon.roam.core/src/org/emoflon/roam/core/gt/GTMapper.java
+++ org.emoflon.roam.core/src/org/emoflon/roam/core/gt/GTMapper.java
@@ -1,5 +1,9 @@
 package org.emoflon.roam.core.gt;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.emoflon.ibex.gt.api.GraphTransformationMatch;
 import org.emoflon.ibex.gt.api.GraphTransformationPattern;
 import org.emoflon.roam.core.RoamEngine;
@@ -8,11 +12,31 @@ import org.emoflon.roam.intermediate.RoamIntermediate.Mapping;
 
 public abstract class GTMapper <GTM extends GTMapping<M,P>, M extends GraphTransformationMatch<M, P>, P extends GraphTransformationPattern<M, P>> extends RoamMapper<GTM> {
 	
+	final protected Map<M, GTM> match2Mappings = Collections.synchronizedMap(new HashMap<>());
+	
 	public GTMapper(final RoamEngine engine, final Mapping mapping) {
 		super(engine, mapping);
 	}
 
 	protected abstract GTM convertMatch(final M match);
+	
+	public void addMapping(final M match) {
+		if(match2Mappings.containsKey(match))
+			return;
+		
+		GTM mapping = convertMatch(match);
+		match2Mappings.put(match, mapping);
+		super.putMapping(mapping);
+	}
+	
+	public void removeMapping(final M match) {
+		GTM mapping = match2Mappings.get(match);
+		if(mapping == null)
+			return;
+		
+		match2Mappings.remove(match);
+		super.removeMapping(mapping);
+	}
 	
 	
 }

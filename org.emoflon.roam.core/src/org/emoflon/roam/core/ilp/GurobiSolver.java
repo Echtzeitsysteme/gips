929dda16e1b1a90d6d2a471efa1b1897201aabf5
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2022-02-15 16:58:04 +0100

    Adds constraint translations for Gurobi

--- org.emoflon.roam.core/src/org/emoflon/roam/core/ilp/GurobiSolver.java
+++ org.emoflon.roam.core/src/org/emoflon/roam/core/ilp/GurobiSolver.java
@@ -1,18 +1,26 @@
 package org.emoflon.roam.core.ilp;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import org.emoflon.roam.core.RoamEngine;
 import org.emoflon.roam.core.RoamMapping;
 import org.emoflon.roam.core.RoamMappingConstraint;
 import org.emoflon.roam.core.RoamMappingObjective;
 import org.emoflon.roam.core.RoamTypeConstraint;
 import org.emoflon.roam.core.RoamTypeObjective;
+import org.emoflon.roam.intermediate.RoamIntermediate.RelationalOperator;
 
+import gurobi.GRB;
 import gurobi.GRB.DoubleParam;
 import gurobi.GRB.IntParam;
 import gurobi.GRBEnv;
+import gurobi.GRBException;
+import gurobi.GRBLinExpr;
 import gurobi.GRBModel;
+import gurobi.GRBVar;
 
-public class GurobiSolver extends ILPSolver{
+public class GurobiSolver extends ILPSolver {
 
 	/**
 	 * Gurobi environment (for configuration etc.).
@@ -24,7 +32,7 @@ public class GurobiSolver extends ILPSolver{
 	 */
 	private GRBModel model;
 
-	public GurobiSolver(RoamEngine engine, final ILPSolverConfig config) throws Exception{
+	public GurobiSolver(final RoamEngine engine, final ILPSolverConfig config) throws Exception {
 		super(engine);
 		env = new GRBEnv("Gurobi_ILP.log");
 		env.set(DoubleParam.TimeLimit, config.timeLimit());
@@ -40,45 +48,140 @@ public class GurobiSolver extends ILPSolver{
 
 	@Override
 	public void solve() {
-		// TODO Auto-generated method stub
-		
+		try {
+			model.update();
+			// TODO: Set optimality tolerance here
+			model.optimize();
+			// TODO: Determine solver status (e.g., 'infeasible')
+		} catch (final GRBException e) {
+			// TODO
+		}
 	}
 
 	@Override
 	public void updateValuesFromSolution() {
 		// TODO Auto-generated method stub
-		
+
+		// TODO: Free model after solution update?
 	}
-	
+
 	@Override
 	protected void translateMapping(RoamMapping mapping) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void translateConstraint(RoamMappingConstraint constraint) {
-		// TODO Auto-generated method stub
-		
+		addIlpConstraintsToGrb(constraint.getConstraints(), constraint.getName());
 	}
 
 	@Override
 	protected void translateConstraint(RoamTypeConstraint constraint) {
-		// TODO Auto-generated method stub
-		
+		addIlpConstraintsToGrb(constraint.getConstraints(), constraint.getName());
 	}
 
 	@Override
 	protected void translateObjective(RoamMappingObjective objective) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void translateObjective(RoamTypeObjective objective) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	//
+	// Helper methods
+	//
+
+	/**
+	 * Adds a given collection of ILP constraints and a given constraint name to the
+	 * Gurobi model.
+	 * 
+	 * @param constraints Collection of integer ILP constraints to add.
+	 * @param name        Name of the overall constraint to add.
+	 */
+	private void addIlpConstraintsToGrb(final Collection<ILPConstraint<Integer>> constraints, final String name) {
+		// For each constraint, create a Gurobi constraint
+		int counter = 0;
+
+		// Have to use an iterator to be able to increment the counter
+		final Iterator<ILPConstraint<Integer>> cnstrsIt = constraints.iterator();
+		while (cnstrsIt.hasNext()) {
+			final ILPConstraint<Integer> curr = cnstrsIt.next();
+			final GRBLinExpr grbLinExpr = new GRBLinExpr();
+
+			//
+			// Operator
+			//
+
+			final char op = convertOperator(curr.operator().getValue());
+
+			//
+			// Terms
+			//
+
+			// Add each term to the GRB linear expression
+			curr.terms().forEach(t -> {
+				try {
+					final GRBVar var = model.addVar(0.0, 1.0, 0.0, GRB.BINARY, t.variable().getName());
+					grbLinExpr.addTerm(t.weight(), var);
+				} catch (final GRBException e) {
+					throw new UnsupportedOperationException(e);
+				}
+			});
+
+			// Add current constructed constraint to the GRB model
+			try {
+				model.addConstr(grbLinExpr, op, curr.constantTerm(), name + "_" + counter++);
+			} catch (final GRBException e) {
+				throw new UnsupportedOperationException(e);
+			}
+		}
+	}
+
+	/**
+	 * Converts a given operator value (from RelationalOperator) to the
+	 * corresponding GRB char value.
+	 * 
+	 * @param op Operator value to convert.
+	 * @return Corresponding GRB char value.
+	 */
+	private char convertOperator(final int op) {
+		switch (op) {
+		case RelationalOperator.LESS_VALUE:
+			// https://www.gurobi.com/documentation/9.5/refman/java_model_addconstr.html
+			throw new UnsupportedOperationException("Gurobi does not support LESS in constraints.");
+		// break;
+		// TODO: Could be expressed with !(x >= a)
+		case RelationalOperator.LESS_OR_EQUAL_VALUE:
+			return GRB.LESS_EQUAL;
+		case RelationalOperator.EQUAL_VALUE:
+			return GRB.EQUAL;
+		case RelationalOperator.GREATER_OR_EQUAL_VALUE:
+			return GRB.GREATER_EQUAL;
+		case RelationalOperator.GREATER_VALUE:
+			throw new UnsupportedOperationException("Gurobi does not support GREATER in constraints.");
+		// break;
+		// TODO: Could be expressed with !(x <= a)
+		case RelationalOperator.NOT_EQUAL_VALUE:
+			throw new UnsupportedOperationException("Gurobi does not support NOT EQUAL in constraints.");
+		// TODO: This introduces an OR constraint
+		// https://math.stackexchange.com/questions/37075/how-can-not-equals-be-expressed-as-an-inequality-for-a-linear-programming-model/1517850
+		// break;
+		default:
+			throw new UnsupportedOperationException("Not yet implemented.");
+		}
+
+		// We do not support strict less-than, strict greater-than, or not-equal
+		// comparators. While these other comparators may seem appropriate for
+		// mathematical programming, we exclude them to avoid potential confusion
+		// related to numerical tolerances.
+		//
+		// https://www.gurobi.com/documentation/9.5/refman/constraints.html
+	}
 
 }

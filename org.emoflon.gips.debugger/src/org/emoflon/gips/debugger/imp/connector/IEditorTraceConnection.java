d2c22c546b6373a996b8c2cf313f8642bf181f1d
MarkBeB <177214717+MarkBeB@users.noreply.github.com>
2025-01-17 00:22:03 +0100

    Update IEditorTraceConnection class description

--- org.emoflon.gips.debugger/src/org/emoflon/gips/debugger/imp/connector/IEditorTraceConnection.java
+++ org.emoflon.gips.debugger/src/org/emoflon/gips/debugger/imp/connector/IEditorTraceConnection.java
@@ -5,6 +5,7 @@ import org.emoflon.gips.debugger.imp.TraceManager;
 /**
  * This represents the connection between an editor and a {@link TraceManager}.
  * A connected editor will send and receive selection events related to tracing.
+ * The implementation may vary between different editors
  */
 public interface IEditorTraceConnection {
 	boolean isConnected();

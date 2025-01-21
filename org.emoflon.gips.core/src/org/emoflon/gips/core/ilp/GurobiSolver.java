1b17755e922cbaa217af7dde207be20d15929312
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2025-01-21 15:05:23 +0100

    Fixes a NPE that occured when the Gurobi env init failed

--- org.emoflon.gips.core/src/org/emoflon/gips/core/ilp/GurobiSolver.java
+++ org.emoflon.gips.core/src/org/emoflon/gips/core/ilp/GurobiSolver.java
@@ -78,7 +78,7 @@ public class GurobiSolver extends ILPSolver {
 		} catch (final Error e) {
 			gurobiInitError = e;
 		}
-		if (!config.enableOutput()) {
+		if (!config.enableOutput() && env != null) {
 			env.set(IntParam.OutputFlag, 0);
 			env.set(IntParam.LogToConsole, 0);
 		}

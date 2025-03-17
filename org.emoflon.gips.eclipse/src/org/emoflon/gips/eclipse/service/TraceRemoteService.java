4ec2017eb14fc23b58948ea9f6c929375725bfdc
MarkBeB <177214717+MarkBeB@users.noreply.github.com>
2025-03-17 13:06:26 +0100

    Rewrite TraceRemoteService
    Plugin should no longer crash when starting two or more runtimes in parallel

--- org.emoflon.gips.eclipse/src/org/emoflon/gips/eclipse/service/TraceRemoteService.java
+++ org.emoflon.gips.eclipse/src/org/emoflon/gips/eclipse/service/TraceRemoteService.java
@@ -1,9 +1,5 @@
 package org.emoflon.gips.eclipse.service;
 
-import java.rmi.AccessException;
-import java.rmi.AlreadyBoundException;
-import java.rmi.NoSuchObjectException;
-import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
@@ -20,14 +16,28 @@ import org.emoflon.gips.eclipse.trace.TraceModelLink;
 
 public final class TraceRemoteService implements ITraceRemoteService {
 
+	public static enum ServiceStatus {
+		RUNNING, PAUSED, ERROR
+	}
+
 	private final IPropertyChangeListener preferenceListener = this::onPreferenceChange;
-	private boolean isRunning = false;
+
 	private Registry registry;
-	private ITraceRemoteService exportedStub;
 	private int port;
+
+	private ServiceStatus status = ServiceStatus.PAUSED;
+	private Exception lastError;
 
 	public TraceRemoteService() throws RemoteException {
 		super();
+	}
+
+	public ServiceStatus getServiceStatus() {
+		return status;
+	}
+
+	public Exception getLastError() {
+		return lastError;
 	}
 
 	public int getPort() {
@@ -37,7 +47,7 @@ public final class TraceRemoteService implements ITraceRemoteService {
 	synchronized private void setPort(int port) {
 		if (port < 0)
 			throw new IllegalArgumentException("port must be greater than or equal to 0");
-		if (isRunning)
+		if (registry != null)
 			throw new UnsupportedOperationException("Unable to change port while service is running");
 
 		this.port = port;
@@ -60,55 +70,51 @@ public final class TraceRemoteService implements ITraceRemoteService {
 	}
 
 	synchronized private void startService() {
-		if (isRunning)
-			return; // nothing to do here
+		if (status == ServiceStatus.RUNNING)
+			return;
 
-		try { // setup RMI service
-			exportedStub = (ITraceRemoteService) UnicastRemoteObject.exportObject(this, 0);
+		try {
+			// this may fail if someone else is already using that port
 			registry = LocateRegistry.createRegistry(port);
-			registry.bind(ITraceRemoteService.SERVICE_NAME, exportedStub);
-		} catch (AccessException e) {
-			throw new IllegalStateException("Unable to start service. Access denied", e);
-		} catch (AlreadyBoundException e) {
-			throw new IllegalStateException("An instance of this service is already running", e);
-		} catch (RemoteException e) {
-			throw new IllegalStateException(e);
-		}
+			UnicastRemoteObject.exportObject(this, 0);
+			registry.bind(ITraceRemoteService.SERVICE_NAME, this);
 
-		isRunning = true;
+			if (lastError != null)
+				System.out.println("GIPSL Eclipse Integration available.");
+
+			lastError = null;
+			status = ServiceStatus.RUNNING;
+		} catch (Exception e) {
+			System.err.println("GIPSL Eclipse Integration unavailable:" + e.getLocalizedMessage());
+			lastError = e;
+			status = ServiceStatus.ERROR;
+		}
 	}
 
 	synchronized private void stopService() {
-		if (!isRunning)
-			return; // nothing to do here
+		if (status == ServiceStatus.PAUSED)
+			return;
 
 		try {
-			UnicastRemoteObject.unexportObject(exportedStub, true);
-			exportedStub = null;
-		} catch (NoSuchObjectException e) {
-			// no such object? No problem!
+			UnicastRemoteObject.unexportObject(this, true);
+		} catch (Exception e) {
+
 		}
 
 		try {
-			registry.unbind(ITraceRemoteService.SERVICE_NAME);
-			registry = null;
-		} catch (AccessException e) {
-			throw new IllegalStateException("Unable to shutdown service. Access denied", e);
-		} catch (NotBoundException e) {
-			// service already unbound? Strange but okay.
-		} catch (RemoteException e) {
-			throw new IllegalStateException(e);
+			UnicastRemoteObject.unexportObject(registry, true);
+		} catch (Exception e) {
+
 		}
 
-		isRunning = false;
+		registry = null;
+		status = ServiceStatus.PAUSED;
 	}
 
 	private void onPreferenceChange(PropertyChangeEvent event) {
 		switch (event.getProperty()) {
 		case PluginPreferences.PREF_TRACE_RMI:
 			boolean shouldServiceBeRunning = ((Boolean) event.getNewValue()).booleanValue();
-			if (this.isRunning == shouldServiceBeRunning)
-				return;
 
 			if (shouldServiceBeRunning)
 				startService();

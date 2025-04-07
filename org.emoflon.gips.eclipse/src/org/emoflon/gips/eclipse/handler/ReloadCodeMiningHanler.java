726e8d1ee06bf3beb93cf2bdc982d01bb0c2ff24
MarkBeB <177214717+MarkBeB@users.noreply.github.com>
2025-04-07 16:11:17 +0200

    Add context cmd to reload code mining

---
+++ org.emoflon.gips.eclipse/src/org/emoflon/gips/eclipse/handler/ReloadCodeMiningHanler.java
@@ -0,0 +1,24 @@
+package org.emoflon.gips.eclipse.handler;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.jface.text.ITextViewer;
+import org.eclipse.jface.text.source.ISourceViewerExtension5;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+public class ReloadCodeMiningHanler extends AbstractHandler implements IHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IEditorPart part = HandlerUtil.getActiveEditorChecked(event);
+		ITextViewer viewer = part.getAdapter(ITextViewer.class);
+		if (viewer instanceof ISourceViewerExtension5 codeMiningViewer)
+			codeMiningViewer.updateCodeMinings();
+
+		return null;
+	}
+
+}

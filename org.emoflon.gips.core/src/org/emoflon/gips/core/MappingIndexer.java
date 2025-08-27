52d2b56c21b56cc8af2bf91f3da70349e4a3f451
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2025-08-27 14:43:13 +0200

    Clean up

--- org.emoflon.gips.core/src/org/emoflon/gips/core/MappingIndexer.java
+++ org.emoflon.gips.core/src/org/emoflon/gips/core/MappingIndexer.java
@@ -11,14 +11,8 @@ import org.emoflon.gips.core.gt.GipsGTMapping;
 
 public class MappingIndexer {
 
-//	final protected GipsMapper<?> mapper;
 	@SuppressWarnings("rawtypes")
 	final protected Map<EObject, Set<GipsGTMapping>> node2mappings = Collections.synchronizedMap(new LinkedHashMap<>());
-
-//	public MappingIndexer(final GipsMapper<?> mapper) {
-//		this.mapper = mapper;
-//		initialize();
-//	}
 
 	public boolean isInitialized() {
 		return !node2mappings.isEmpty();
@@ -37,14 +31,6 @@ public class MappingIndexer {
 		}
 		return query;
 	}
-
-//	private void initialize() {
-//		// TODO
-//		mapper.getMappings().forEach((name, mapping) -> {
-//			final GipsGTMapping<?, ?> m = (GipsGTMapping<?, ?>) mapping;
-//			m.getMatch().getPattern().getPatternSet();
-//		});
-//	}
 
 	@SuppressWarnings("rawtypes")
 	public Set<GipsGTMapping> getMappingsOfNodes(final Set<EObject> nodes) {

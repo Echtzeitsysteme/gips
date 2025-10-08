b4cfdad42f757486567d140ccee7dfd8010456b9
MarkBeB <177214717+MarkBeB@users.noreply.github.com>
2025-10-08 12:31:30 +0200

    Add JavaDoc to PatternMatch2MappingSorter

--- org.emoflon.gips.core/src/org/emoflon/gips/core/gt/PatternMatch2MappingSorter.java
+++ org.emoflon.gips.core/src/org/emoflon/gips/core/gt/PatternMatch2MappingSorter.java
@@ -1,6 +1,5 @@
 package org.emoflon.gips.core.gt;
 
-import java.util.Collection;
 import java.util.List;
 
 import org.emoflon.ibex.gt.api.GraphTransformationMatch;
@@ -8,12 +7,23 @@ import org.emoflon.ibex.gt.api.GraphTransformationMatch;
 public interface PatternMatch2MappingSorter {
 
 	/**
+	 * Performs a sorting operation just before the matches are
+	 * {@link GipsPatternMapper#addMapping(GraphTransformationMatch) added} to the
+	 * {@link GipsPatternMapper mapper}. The order of the elements affects the
+	 * variable names assigned to them and may affect their order of appearance in
+	 * any output.
+	 * <p>
+	 * <b>Note</b>:
+	 * <ul>
+	 * <li>The list of matches provided can be safely sorted and returned
+	 * <li>Any returned match will be added to the mapper
+	 * <li>Mapper is provided for context reason
+	 * </ul>
 	 * 
-	 * @param <M>
-	 * @param mapper
-	 * @param matches
-	 * @return
+	 * @param mapper  which provides the matches, not null
+	 * @param matches a list of matches, not null, contains no null elements
+	 * @return a sorted list of matches, not null, contains no null elements
 	 */
-	<M extends GraphTransformationMatch<M, ?>> Collection<M> sort(GipsPatternMapper<?, M, ?> mapper, List<M> matches);
+	<M extends GraphTransformationMatch<M, ?>> List<M> sort(GipsPatternMapper<?, M, ?> mapper, List<M> matches);
 
 }
--- org.emoflon.gips.core/src/org/emoflon/gips/core/gt/PatternMatch2MappingSorterByURI.java
+++ org.emoflon.gips.core/src/org/emoflon/gips/core/gt/PatternMatch2MappingSorterByURI.java
@@ -12,11 +12,41 @@ import org.emoflon.ibex.gt.api.GraphTransformationMatch;
 import org.emoflon.ibex.patternmodel.IBeXPatternModel.IBeXContextPattern;
 import org.emoflon.ibex.patternmodel.IBeXPatternModel.IBeXNode;
 
+/**
+ * Sorts a list of matches based on the
+ * {@link EcoreUtil#getRelativeURIFragmentPath(EObject, EObject) relative URI}
+ * of each node within a match.
+ * <p>
+ * Matches are found in a non-deterministic order. This means that, for multiple
+ * runs with no change in the input, a match can have different variable names.
+ * While this is not a problem, it makes it harder for humans to understand the
+ * relationship between two different outputs with the same input.
+ * <p>
+ * The idea behind this sorter is to sort matches based on their 'location'
+ * within the tree. A match is composed of one or more nodes. The 'location' of
+ * a single node is defined by its relative path from the root.
+ * <p>
+ * To compare two matches, we compare the locations of their nodes. The order in
+ * which the nodes are compared is defined by the names of the nodes, as defined
+ * by the pattern on which a match is based.
+ * <p>
+ * Steps:
+ * <ul>
+ * <li>All of the matches provided belong to the same pattern.
+ * <li>Retrieve the node names from the pattern and
+ * {@link String#compareTo(String) sort} them.
+ * <li>Compare the matches pairwise; iterate through the nodes in the given
+ * order and {@link String#compareTo(String) compare} them based on their
+ * relative URIs.
+ * <li>If two relative URIs are the same, continue with the next node.
+ * <li>Otherwise sort both matches based on the result.
+ * <li>Repeat until matches are sorted.
+ * </ul>
+ */
 public class PatternMatch2MappingSorterByURI implements PatternMatch2MappingSorter {
 
 	@Override
-	public <M extends GraphTransformationMatch<M, ?>> Collection<M> sort(GipsPatternMapper<?, M, ?> mapper,
-			List<M> matches) {
+	public <M extends GraphTransformationMatch<M, ?>> List<M> sort(GipsPatternMapper<?, M, ?> mapper, List<M> matches) {
 
 		if (matches.isEmpty())
 			return matches;
@@ -57,10 +87,10 @@ public class PatternMatch2MappingSorterByURI implements PatternMatch2MappingSort
 
 					if (pathA == pathB) {
 						continue;
-					} else if (pathA == null) {
-						return 1;
 					} else if (pathB == null) {
-						return -1;
+						return -1; // nodeA != null && nodeB == null
+					} else if (pathA == null) {
+						return 1; // nodeA == null && nodeB != null
 					}
 
 					var result = pathA.compareTo(pathB);

c007bf3475456e599b8b57b3cc0592b8d4c45fc8
Maximilian Kratz <maximilian.kratz@es.tu-darmstadt.de>
2026-03-05 16:07:52 +0100

    Adds removed constraints stats to the output of `solveProblem(Timed)`

--- org.emoflon.gips.core/src/org/emoflon/gips/core/milp/ProblemStatistics.java
+++ org.emoflon.gips.core/src/org/emoflon/gips/core/milp/ProblemStatistics.java
@@ -1,5 +1,45 @@
 package org.emoflon.gips.core.milp;
 
-public record ProblemStatistics(long mappings, long vars, long constraints) {
+public class ProblemStatistics {
+
+	final public long mappings;
+	final public long vars;
+	final public long constraints;
+	private long removedDuplicateConstraints = 0;
+	private long removedTrivialConstraints = 0;
+
+	public ProblemStatistics(final long mappings, final long vars, final long constraints) {
+		if (mappings < 0 || vars < 0 || constraints < 0) {
+			throw new IllegalArgumentException("Given value was smaller than 0, which is not supported.");
+		}
+
+		this.mappings = mappings;
+		this.vars = vars;
+		this.constraints = constraints;
+	}
+
+	public void setRemovedDuplicateConstraints(final long removedDuplicateConstraints) {
+		if (removedDuplicateConstraints < 0) {
+			throw new IllegalArgumentException("Given value was smaller than 0, which is not supported.");
+		}
+
+		this.removedDuplicateConstraints = removedDuplicateConstraints;
+	}
+
+	public long getRemovedDuplicateConstraints() {
+		return this.removedDuplicateConstraints;
+	}
+
+	public void setRemovedTrivialConstraints(final long removedTrivialConstraints) {
+		if (removedTrivialConstraints < 0) {
+			throw new IllegalArgumentException("Given value was smaller than 0, which is not supported.");
+		}
+
+		this.removedTrivialConstraints = removedTrivialConstraints;
+	}
+
+	public long getRemovedTrivialConstraints() {
+		return this.removedTrivialConstraints;
+	}
 
 }
